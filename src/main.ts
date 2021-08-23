54008368ef202458ae83a6bde1a8da65672e6241
uphy <yuhi.ishikura@gmail.com>
2021-08-23 20:33:12 +0900

    Show reminders in calendar.

--- src/main.ts
+++ src/main.ts
@@ -69,7 +69,7 @@ export default class ReminderPlugin extends Plugin {
       this.editDetector.fileChanged();
     });
     this.registerCodeMirror((cm: CodeMirror.Editor) => {
-      const dateTimeChooser = new DateTimeChooserView(cm);
+      const dateTimeChooser = new DateTimeChooserView(cm, this.reminders);
       cm.on(
         "change",
         (cmEditor: CodeMirror.Editor, changeObj: CodeMirror.EditorChange) => {
